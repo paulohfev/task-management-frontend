b8a823f1fe79707cee1476cdeb63a54e37da2c47
paulohfev <paulohfev@users.noreply.github.com>
2022-11-06 01:50:45 +0000

    deploy: 8bb53a4ba48f433aa7c55dabc2232a2e96f143cf

---
+++ precache-manifest.92a8dd2f70b0c7755eb16bea485285c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42034679ca9e748426a7020be66ecdf4",
+    "url": "https://paulohfev.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "f0b4c50ab6d8f95c601f",
+    "url": "https://paulohfev.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "d06303df21b562d656c5",
+    "url": "https://paulohfev.github.io/task-management-frontend/static/js/2.35271f9a.chunk.js"
+  },
+  {
+    "revision": "f0b4c50ab6d8f95c601f",
+    "url": "https://paulohfev.github.io/task-management-frontend/static/js/main.19e6bd50.chunk.js"
+  },
+  {
+    "revision": "f4822cc4e29d278cf467",
+    "url": "https://paulohfev.github.io/task-management-frontend/static/js/runtime~main.5985b171.js"
+  }
+]);
